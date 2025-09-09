f4827eb067df94ebddf921718b88ca9081274979
Dubass83 <makssych@gmail.com>
2025-09-09 21:02:10 +0300

    chore: add validation for comment creation fields

--- cmd/api/comments.go
+++ cmd/api/comments.go
@@ -7,8 +7,8 @@ import (
 )
 
 type commentPayLoad struct {
-	UserID  int64  `json:"user_id"`
-	Content string `json:"content"`
+	UserID  int64  `json:"user_id" validate:"required"`
+	Content string `json:"content" validate:"required"`
 }
 
 func (app *application) CreateCommentToPostByIDHandler(w http.ResponseWriter, r *http.Request) {
@@ -21,13 +21,18 @@ func (app *application) CreateCommentToPostByIDHandler(w http.ResponseWriter, r 
 		internalServerError(w, r, err)
 	}
 
+	if err := validate.Struct(payload); err != nil {
+		badRequestResponse(w, r, err)
+		return
+	}
+
 	comment := &store.Comment{
 		UserID:  payload.UserID,
 		Content: payload.Content,
 		PostID:  post.ID,
 	}
-	err := app.store.Comment.Create(ctx, comment)
-	if err != nil {
+
+	if err := app.store.Comment.Create(ctx, comment); err != nil {
 		internalServerError(w, r, err)
 	}
 
