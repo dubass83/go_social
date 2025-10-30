57b1a2b259cbaab768602977bad4667c96a4c705
Dubass83 <makssych@gmail.com>
2025-10-30 20:37:38 +0200

    feat: finished web confirmation page
    Signed-off-by: Dubass83 <makssych@gmail.com>

---
+++ web/src/ConfirmationPage.tsx
@@ -0,0 +1,26 @@
+import { useParams } from "react-router-dom";
+import { API_URL } from "./config";
+
+export const ConfirmationPage = () => {
+  const { token } = useParams<{ token: string }>();
+  const handleConfirm = async () => {
+    const response = await fetch(`${API_URL}/users/activate/${token}`, {
+      method: "PUT",
+    });
+
+    if (response.ok) {
+      // Handle successful activation
+    } else {
+      // Handle error
+      alert("An error occurred while confirming your account.");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Confirmation</h1>
+      <p>Thank you for your registration!</p>
+      <button onClick={handleConfirm}>Confirm</button>
+    </div>
+  );
+};
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
+import { ConfirmationPage } from "./ConfirmationPage.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/confirm/:token",
+    element: <ConfirmationPage />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
