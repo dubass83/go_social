bfd9d5d301d298ab99b6c88203933d09440a90fb
Dubass83 <makssych@gmail.com>
2025-11-30 10:57:20 +0200

    fix: add err logs into auth middelware
    Signed-off-by: Dubass83 <makssych@gmail.com>

--- cmd/api/middelware.go
+++ cmd/api/middelware.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/rs/zerolog/log"
 )
 
 func (app *application) BasicAuthMiddleware(next http.Handler) http.Handler {
@@ -25,26 +26,31 @@ func (app *application) AuthTokenMiddelware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
 		if authHeader == "" {
+			log.Error().Msg("Authorization header is missing")
 			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 			return
 		}
 		parts := strings.Split(authHeader, " ")
 		if len(parts) != 2 {
+			log.Error().Msg("Invalid authorization header format")
 			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 			return
 		}
 		if parts[0] != "Bearer" {
+			log.Error().Msg("Authorization header must start with 'Bearer' but got '" + parts[0] + "'")
 			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 			return
 		}
 		token := parts[1]
 		jwtToken, err := app.authenticator.ValidateToken(token)
 		if err != nil {
+			log.Error().Err(err).Msg("Invalid token")
 			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 			return
 		}
 
 		if !jwtToken.Valid {
+			log.Error().Msg("Token is not valid")
 			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 			return
 		}
@@ -55,6 +61,7 @@ func (app *application) AuthTokenMiddelware(next http.Handler) http.Handler {
 
 		user, err := app.store.User.GetByID(ctx, userID)
 		if err != nil {
+			log.Error().Err(err).Msg("Failed to get user")
 			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 			return
 		}
