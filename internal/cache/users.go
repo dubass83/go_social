8610b3a5823b5b72d15c4cfbfa23ebb83bd14f46
Dubass83 <makssych@gmail.com>
2026-01-18 17:21:31 +0200

    fix: if Redis client is not configured, return cache miss and Marshal/Unmarshal user
    Signed-off-by: Dubass83 <makssych@gmail.com>

--- internal/cache/users.go
+++ internal/cache/users.go
@@ -2,12 +2,14 @@ package cache
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"strconv"
 	"time"
 
 	"github.com/dubass83/go_social/internal/store"
 	"github.com/go-redis/redis/v8"
+	"github.com/rs/zerolog/log"
 )
 
 const (
@@ -37,16 +39,53 @@ func (uch *userCache) getUserKey(id int64) string {
 }
 
 func (uch *userCache) Get(ctx context.Context, id int64) (*store.User, error) {
-	user := &store.User{}
-	idStr := uch.getUserKey(id)
-	err := uch.rdb.Get(ctx, idStr).Scan(user)
+	// If Redis client is not configured, return cache miss
+	if uch.rdb == nil {
+		return nil, nil
+	}
+
+	key := uch.getUserKey(id)
+
+	// Get the JSON string from Redis
+	data, err := uch.rdb.Get(ctx, key).Bytes()
 	if err == redis.Nil {
+		// Cache miss - not an error, just return nil
+		log.Debug().Str("key", key).Msg("Cache miss")
+		return nil, nil
+	}
+	if err != nil {
 		return nil, fmt.Errorf("failed to get user from cache: %w", err)
 	}
-	return user, nil
+
+	// Unmarshal JSON into user struct
+	var user store.User
+	if err := json.Unmarshal(data, &user); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal user from cache: %w", err)
+	}
+
+	log.Debug().Int64("user_id", id).Str("username", user.Username).Msg("Cache hit")
+	return &user, nil
 }
 
 func (uch *userCache) Set(ctx context.Context, user *store.User) error {
-	idStr := uch.getUserKey(user.ID)
-	return uch.rdb.Set(ctx, idStr, user, uch.ttl).Err()
+	// If Redis client is not configured, silently skip
+	if uch.rdb == nil {
+		return nil
+	}
+
+	key := uch.getUserKey(user.ID)
+
+	// Marshal user to JSON
+	data, err := json.Marshal(user)
+	if err != nil {
+		return fmt.Errorf("failed to marshal user for cache: %w", err)
+	}
+
+	// Store in Redis with TTL
+	if err := uch.rdb.Set(ctx, key, data, uch.ttl).Err(); err != nil {
+		return fmt.Errorf("failed to set user in cache: %w", err)
+	}
+
+	log.Debug().Int64("user_id", user.ID).Str("username", user.Username).Msg("User cached")
+	return nil
 }
